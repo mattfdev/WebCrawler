96116f145c6c8615ff913f54f5411ba1860f7d66
Matthew Francis <matthew_francis@outlook.com>
2017-11-25 16:21:03 -0500

    Updated basic crawler capabilities to go out and search the web for relevant links, outputting any found to stdout

--- src/main/java/com/ryerson/Crawler.java
+++ src/main/java/com/ryerson/Crawler.java
@@ -6,63 +6,71 @@ import org.jsoup.select.Elements;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Hello world!
  *
  */
-public class Crawler
-{
+public class Crawler {
+
+    private static final int numberOfHosts = 300;
+    private static final int maxiumCrawlDepth = 4;
+    private static final int maxiumumCrawledPages = 500;
+    private static String[] searchQuery;
+
 
     public static void main( String[] args ) throws IOException {
         Set<String> initialSeed = initializeWebCrawlerSeed();
-        System.out.println(initialSeed.size());
         for (String link : initialSeed) {
-            System.out.println(link);
+            Scraper linkFollower = new Scraper(link, searchQuery);
+            Elements links = linkFollower.processBaseUrlPage();
+            for (Element elem : links) {
+                System.out.println("Link is " + elem.attr("href")+ " : " + elem.html());
+            }
         }
     }
 
     /**
      * Retrieve top 100 results from Bing for a user inputted query.
-     * @return set containg urls of the top 100 resutls of a search term.
+     * @return set containing urls of the top 100 results of a search term.
      */
     private static Set<String> initializeWebCrawlerSeed() {
         StringBuilder initialSeedUrl = new StringBuilder("https://www.bing.com/search?q=");
-        Set<String> unique_links = new HashSet<String>();
+        Set<String> unique_links = new TreeSet<String>();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Please enter a search term to crawl the web for: ");
         try {
             String userInput = reader.readLine();
-            String[] termsToSearch = userInput.split(" ");
-            for (int i = 0; i < termsToSearch.length; i++) {
-                initialSeedUrl.append(termsToSearch[i]);
-                if (i == termsToSearch.length - 1) continue;
+            searchQuery = userInput.split(" ");
+
+            for (int i = 0; i < searchQuery.length; i++) {
+                initialSeedUrl.append(searchQuery[i]);
+                if (i == searchQuery.length - 1) continue;
                 initialSeedUrl.append("+");
             }
             initialSeedUrl.append("&count=50");
 
-            Scraper initialScraper = new Scraper();
-            Elements seedResults = initialScraper.retrieveInitialSeedResults(initialSeedUrl.toString());
+            Scraper initialScraper = new Scraper(initialSeedUrl.toString(), searchQuery);
+            Elements seedResults = initialScraper.retrieveInitialSeedResults();
 
-            // Retrieve another 50 results and combine all 100 results into a single elements arraylist.
-            initialSeedUrl.append("&first=51&FORM=PORE");
-            Elements additionalResults = initialScraper.retrieveInitialSeedResults(initialSeedUrl.toString());
+            // Retrieve another 50 results and combine all 100 results into a single elements array list.
+            initialScraper.setBaseUrl(initialSeedUrl.append("&first=51&FORM=PORE").toString());
+            Elements additionalResults = initialScraper.retrieveInitialSeedResults();
             seedResults.addAll(additionalResults);
 
             System.out.println("The number of results collected for the query is: " + seedResults.size());
-            for (Element link : seedResults) {
-                //System.out.println("Link is " + link.attr("href")+ "and inner text is " + link.html());
-                unique_links.add(link.attr("href"));
-            }
+            unique_links.addAll(seedResults.select("a").eachAttr("href"));
             return unique_links;
         } catch (IOException ex) {
             System.out.println("Encountered IOException: " + ex);
             return unique_links;
         }
     }
+
+
 
 
 }
--- src/main/java/com/ryerson/Scraper.java
+++ src/main/java/com/ryerson/Scraper.java
@@ -2,34 +2,80 @@ package com.ryerson;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class Scraper {
 
-    Scraper() {
+    public String getBaseUrl() {
+        return baseUrl;
+    }
 
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    private final String[] searchQuery;
+    private String baseUrl;
+
+    Scraper(String urlToParse, String[] query) {
+        baseUrl = urlToParse;
+        searchQuery = query;
     }
 
     /**
-     * Retrieve an arrray of HTML link elements for a given url. Hard coded to process Bing results, and filter out irrelevant elements.
-     * @param url
-     * @return
+     * Retrieve an arrray of HTML link elements for a given baseUrl. Hard coded to process Bing results, and filter out irrelevant elements.
+     * @return an array of elements that point to HTML links.
      */
-    public Elements retrieveInitialSeedResults(String url) {
+    public Elements retrieveInitialSeedResults() {
         try {
-            Document document = Jsoup.connect(url).get();
-            //document.select("a[href*=#]").remove();
+            Document document = Jsoup.connect(baseUrl).get();
+            document.select("a[href*=#]").remove();
             document.select("a[id]").remove();
+            // Filter out bing ad content.
             document.select("a[href*=bat.bing.com]").remove();
+            document.select("a[href*=advertise.bingads]").remove();
+            document.select("a[href*=go.microsoft]").remove();
             document.select("a[href*=choice.microsoft]").remove();
-            Elements links = document.select("a[href*=http]"); // a with href
-            return links;
+            return document.select("a[href*=http]"); // a with href
         } catch (IOException ex) {
             System.out.println("Exception encountered : " + ex);
             return null;
         }
+    }
+
+    /**
+     * Store HTMl page in database, and return any potentially intresting links to future sites to crawl.
+     * @return possibly relevant urls to add to crawling queue.
+     */
+    public Elements processBaseUrlPage() {
+        try {
+            Document webpage = Jsoup.connect(baseUrl).get();
+            // Store in DB
+            return filterEncountedUrls(webpage.select("a[href*=http]"));
+        } catch (Exception ex) {
+            System.out.println("Error occurred when trying to parse wepage " + baseUrl+ " : " + ex);
+            return null;
+        }
+    }
+
+    // Filter out irrelevant, and internally referring links.
+    private Elements filterEncountedUrls(Elements htmlLinks) {
+        Iterator<Element> elementFinder = htmlLinks.iterator();
+        while(elementFinder.hasNext()) {
+            Element link = elementFinder.next();
+            String anchorText = link.html();
+            String anchorLink = link.attr("href");
+            boolean relevantLink = Arrays.stream(searchQuery).anyMatch(anchorText::contains)
+                    || Arrays.stream(searchQuery).anyMatch(anchorLink::contains);
+            boolean internalLink = anchorLink.contains(baseUrl);
+            if (!relevantLink || internalLink) elementFinder.remove();
+        }
+        return htmlLinks;
     }
 }
 
